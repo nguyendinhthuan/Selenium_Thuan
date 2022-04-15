f7e8631f43f999f479e3f35479f5828da14e6e1e
nguyendinhthuan <55021180+nguyendinhthuan@users.noreply.github.com>
2022-04-15 15:38:00 +0700

    Add files via upload

---
+++ general-java-automation-master/src/main/java/com/automation/App.java
@@ -0,0 +1,18 @@
+package com.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://google.com");
+    }
+}
---
+++ general-java-automation-master/src/test/resources/features/first.feature
@@ -0,0 +1,8 @@
+@Regression
+Feature: Feature name
+    Feature Description
+
+    Scenario: Test new Scenario
+    Given User opens Browser successfully
+    # When User goes to the URL
+    # Then User should see the title of the page
